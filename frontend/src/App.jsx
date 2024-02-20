711a9406aa7ad3af6c24d1196abaa1228ee99e57
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-20 20:06:59 +0200

    Add header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>MERN Auth</Navbar.Brand>
+          <Navbar.Toggle arial-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/login'>
+                <FaSignInAlt /> Sign in
+              </Nav.Link>
+              <Nav.Link href='/register'>
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
