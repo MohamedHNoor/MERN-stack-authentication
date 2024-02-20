bc2b9668c7d027d0fa896672e2224de4a492bd69
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-20 14:59:23 +0200

    import dependencies, start app and connected to database

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './db/connect.js';
+import userRoutes from './routes/userRoutes.js';
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => res.send('server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+  }
+};
+
+start();
