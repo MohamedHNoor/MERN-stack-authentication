d6b4c7f932a3b6f95b87db156b10cadf68ec9571
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-20 17:20:56 +0200

    import cookie parser into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './db/connect.js';
 import userRoutes from './routes/userRoutes.js';
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
