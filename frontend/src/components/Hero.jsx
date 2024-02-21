d98b59e45c335abb5435eb18f167153372445988
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-21 11:53:46 +0200

    Create Hero component

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
+
+const Hero = () => {
+  return (
+    <div className=' py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+          <h1 className='text-center mb-4'>MERN Authentication</h1>
+          <p className='text-center mb-4'>
+            MERN authentication that stores a JWT in an HTTP-Only cookie. It's
+            built with React, Node.js, Express, MongoDB, Redux Toolkit and the
+            React Bootstrap library
+          </p>
+          <div className='d-flex'>
+            <Button variant='primary' href='/login' className='me-3'>
+              Sign In
+            </Button>
+            <Button variant='secondary' href='/register'>
+              Register
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
