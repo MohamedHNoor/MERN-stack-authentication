12b710e4ab0799e4d3096dd111e2829f743889dd
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-24 11:13:54 +0200

    add login route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 );
