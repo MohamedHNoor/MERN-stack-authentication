3ff8bcbfc1fd2914b3f1ce8cfc3e2814cdf62424
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-25 13:49:30 +0200

    add auth reducer to redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/AuthSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true,
 });
