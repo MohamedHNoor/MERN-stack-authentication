eed06c070264aaffcb9a2ff7b815924fdedf7b13
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-25 14:00:20 +0200

    create apiSlice reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
 import authReducer from './slices/AuthSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
