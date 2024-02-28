528b08cfc3efb9b9ae00053807d6de87dc2f1d11
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-28 16:59:54 +0200

    updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './db/connect.js';
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 const port = process.env.PORT || 8000;
 
@@ -16,7 +17,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('server is ready'));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
